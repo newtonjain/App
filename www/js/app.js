b7549da2d84b3e7b02871c951b0c52e8a1b54431
Newton Jain <newton.jain@gmail.com>
2016-01-26 14:37:23 -0800

    commiting bad changes

--- www/js/app.js
+++ www/js/app.js
@@ -22,13 +22,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', "
     } 
     
     // Register to hockey app
-    /*
+    
     hockeyapp.start(function() {
         console.log("success");
     }, function (err) {
         console.log("error:" + err);
-    }, "0266c81b31fd4c588f573718e0bf47eb", true);
-    */    
+    }, "ba8733f5576a48a2b25cb0737ebf386d", true);
+        
   });   
 })
 
